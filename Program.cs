fd2cc30aa1be08fb5f1e16c63ea168c0fc72fb73
Jack <96661942+jpshrader@users.noreply.github.com>
2023-11-14 19:19:05 -0500

    nvm

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace github_whitelist {
+    class Program {
+        static int Main(string[] args) {
+            if (!args.Any()) {
+                Console.WriteLine("received no arguments");
+                return -1;
+            }
+
+            foreach (var arg in args) {
+                Console.WriteLine($"arg: {arg}");
+            }
+
+            return 0;
+        }
+    }
+}
