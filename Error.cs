f3e2b0cc94c7a80eab5d05be83e65401d524d083
Jack <96661942+jpshrader@users.noreply.github.com>
2024-01-01 17:45:35 -0500

    Update Error.cs

--- Error.cs
+++ Error.cs
@@ -1,33 +1,25 @@
 namespace github_whitelist {
     public abstract class Error {
         public abstract string Message { get; }
-    }
-
-    public class NotFoundError(string entityType) : Error {
-        public override string Message => $"could not retrieve {entityType}";
 
         public override string ToString() {
             return Message;
         }
+    }
+
+    public class NotFoundError(string entityType) : Error {
+        public override string Message => $"could not retrieve {entityType}";
     }
 
     public class CoundNotDeleteError(string entityType, string id) : Error {
         public override string Message => $"could not delete {entityType}: {id}";
 
         public string Id => id;
-
-        public override string ToString() {
-            return Message;
-        }
     }
 
     public class CoundNotCreateError(string entityType, string id) : Error {
         public override string Message => $"could not create {entityType}: {id}";
 
         public string Id => id;
-
-        public override string ToString() {
-            return Message;
-        }
     }
 }
