65fd899989f394b1f1718d4f09cc5436937d0579
Jack <96661942+jpshrader@users.noreply.github.com>
2024-01-20 19:19:33 -0500

    add purge

--- Program.cs
+++ Program.cs
@@ -22,6 +22,15 @@ namespace github_whitelist {
                 return -1;
             }
 
+            var purge = false;
+            if (args.Length > 2) {
+                var parsed = bool.TryParse(args[2], out purge);
+                if (!parsed) {
+                    Console.WriteLine("purge must be a boolean");
+                    return -1;
+                }
+            }
+
             var client = new OctokitClient(token);
 
             var (allowList, ipListErr) = await client.GetIpAllowList(orgSlug);
@@ -37,11 +46,24 @@ namespace github_whitelist {
                 return -1;
             }
 
+            if (purge) {
+                Console.WriteLine("purging allow list");
+                await PurgeAllowList(client, allowList, managedAllowListItems, nodes);
+            } else {
+                Console.WriteLine("adding missing nodes to allow list");
+                await AddMissingNodes(orgSlug, client, allowList, managedAllowListItems, nodes);
+            }
+
+            Console.WriteLine("done");
+            return 0;
+        }
+
+        private static async Task AddMissingNodes(string orgSlug, OctokitClient client, ImmutableArray<AllowedIp> allowList, ImmutableArray<AllowedIp> managedAllowListItems, ImmutableArray<string> nodes){
             var managedItemsToDelete = managedAllowListItems
                 .Where(i => !nodes.Contains(i.Cidr))
                 .ToImmutableArray();
 
-            Console.WriteLine($"found {nodes.Length} github action nodes");
+            Console.WriteLine($"found {nodes.Length} git hub action nodes");
             Console.WriteLine($"found {managedAllowListItems.Length} managed allow list entires");
             Console.WriteLine($"found {allowList.Length - managedAllowListItems.Length} other entires in the allow list");
             Console.WriteLine($"attempting to delete {managedItemsToDelete.Length} managed allow list entires");
@@ -50,7 +72,7 @@ namespace github_whitelist {
             var numFailedToDelete = managedItemsToDelete.Length - deletedItems.Length;
             if (numFailedToDelete > 0) {
                 Console.WriteLine($"failed to delete {numFailedToDelete} managed entries");
-                return -1;
+                return;
             }
 
             Console.WriteLine($"deleted {managedItemsToDelete.Length} managed allow list entires");
@@ -73,13 +95,27 @@ namespace github_whitelist {
             var numFailedToCreate = newAllowListItems.Length - addedIps.Length;
             if (numFailedToCreate > 0) {
                 Console.WriteLine($"failed to add {numFailedToCreate} managed entries");
-                return -1;
+                return;
             }
 
             Console.WriteLine($"added {addedIps.Length} managed allow list entries");
             Console.WriteLine($"kept {nodes.Length - nodesToAdd.Length} managed allow list entries");
-            Console.WriteLine("done");
-            return 0;
+        }
+
+        private static async Task PurgeAllowList(OctokitClient client, ImmutableArray<AllowedIp> allowList, ImmutableArray<AllowedIp> managedAllowListItems, ImmutableArray<string> nodes){
+            Console.WriteLine($"found {nodes.Length} git hub action nodes");
+            Console.WriteLine($"found {managedAllowListItems.Length} managed allow list entires");
+            Console.WriteLine($"found {allowList.Length - managedAllowListItems.Length} other entires in the allow list");
+            Console.WriteLine($"attempting to purge {managedAllowListItems.Length} managed allow list entires");
+
+            var deletedItems = await client.DeleteIpAllowListItems(managedAllowListItems);
+            var numFailedToDelete = managedAllowListItems.Length - deletedItems.Length;
+            if (numFailedToDelete > 0) {
+                Console.WriteLine($"failed to delete {numFailedToDelete} managed entries");
+                return;
+            }
+
+            Console.WriteLine($"deleted {managedAllowListItems.Length} managed allow list entires");
         }
     }
 }
